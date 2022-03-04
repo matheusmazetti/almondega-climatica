72859468ea2c6aae8f5860f5f69c4069d48bd615
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-03-04 19:04:12 -0300

    solicitar cidade

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let positionName = null
 function sucess(position){
     console.log(position);
 }
+function errorCallback(PositionError){
+    console.log(PositionError);
+    positionName = prompt("Digite o nome da cidade:");
+}
+navigator.geolocation.getCurrentPosition(sucess, errorCallback);
 
-navigator.geolocation.getCurrentPosition(sucess);
