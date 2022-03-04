5840ffb5d57643168a7b0593e21ef90086242d07
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-03-04 19:12:41 -0300

    acessar temperatura

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let positionName = null
 function sucess(position){
     console.log(position);
+    return position;
 }
 function errorCallback(PositionError){
     console.log(PositionError);
@@ -8,3 +9,12 @@ function errorCallback(PositionError){
 }
 navigator.geolocation.getCurrentPosition(sucess, errorCallback);
 
+function getWeather(){
+    let promisse = axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=f25110b0f83adb9f7c080ee182cd1d00`);
+    promisse.then(weatherSucess);
+    promisse.catch(weatherError);
+}
+
+function weatherError(errorCode){
+    
+}
